eec0d8c93dc4263b4622a599d2961d8fe9821cfe
yeji <yejiyoun@sookmyung.ac.kr>
2021-06-30 17:55:58 +0900

    tab 구현

---
+++ app/src/main/java/com/example/madcamp1/Fragment1.java
@@ -0,0 +1,20 @@
+package com.example.madcamp1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/madcamp1/MainActivity.java
+++ app/src/main/java/com/example/madcamp1/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.madcamp1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment fragment1 ,fragment2, fragment3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, fragment1).commit();
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // TODO : process tab selection event.
+                int pos = tab.getPosition();
+
+                Fragment selected=null;
+                if(pos==0){
+                    selected = fragment1;
+                }
+                else if(pos==1){
+                    selected = fragment2;
+                }
+                else if(pos==2){
+                    selected = fragment3;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+        }) ;
     }
 }

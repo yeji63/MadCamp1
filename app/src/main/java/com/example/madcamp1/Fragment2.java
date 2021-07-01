3f1cbd2e00dc7cf72374522471dfff2ffb935007
yeji <yejiyoun@sookmyung.ac.kr>
2021-07-01 14:33:03 +0900

    tab2 갤러리

--- app/src/main/java/com/example/madcamp1/Fragment2.java
+++ app/src/main/java/com/example/madcamp1/Fragment2.java
@@ -1,19 +1,44 @@
 package com.example.madcamp1;
-
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.io.File;
 
 public class Fragment2 extends Fragment {
+    private Integer[] flowers = {R.drawable.daisy1, R.drawable.daisy2, R.drawable.daisy3, R.drawable.daisy4, R.drawable.daisy5};
+
+    public String basePath = null;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        GridView gridView = (GridView) view.findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(getContext()));
+
+
+//        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+//                "MyCameraApp");
+//        if (! mediaStorageDir.exists()){
+//            if (! mediaStorageDir.mkdirs()){
+//                Log.d("MyCameraApp", "failed to create directory");
+//            }
+//        }
+
+        //basePath = mediaStorageDir.getPath();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/madcamp1/ImageAdapter.java
@@ -0,0 +1,49 @@
+package com.example.madcamp1;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public Integer[] mThumbIds={
+            R.drawable.daisy5, R.drawable.daisy4, R.drawable.daisy3, R.drawable.daisy2, R.drawable.daisy1,
+            R.drawable.dandelion1, R.drawable.dandelion2, R.drawable.dandelion3, R.drawable.dandelion4, R.drawable.dandelion5,
+            R.drawable.rose1, R.drawable.rose2, R.drawable.rose3, R.drawable.rose4, R.drawable.rose5,
+            R.drawable.sunflower1, R.drawable.sunflower2, R.drawable.sunflower3, R.drawable.sunflower4, R.drawable.sunflower5
+    };
+
+    public ImageAdapter(Context c){
+        mContext=c;
+    }
+
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(300, 300));
+        return imageView;
+    }
+}

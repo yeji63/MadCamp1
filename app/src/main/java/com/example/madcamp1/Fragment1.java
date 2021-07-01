a661c4e75f149ddf1f98ea309f34c954e6fc0e43
fbtngus <702fbtngus@kaist.ac.kr>
2021-07-01 15:29:37 +0900

    Link with phone contact

--- app/src/main/java/com/example/madcamp1/Fragment1.java
+++ app/src/main/java/com/example/madcamp1/Fragment1.java
@@ -1,13 +1,21 @@
 package com.example.madcamp1;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +27,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 public class Fragment1 extends Fragment {
     public ArrayList<contact> dataList;
@@ -27,13 +36,75 @@ public class Fragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle   savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_1, container, false);
 
-        jsonParsing(getJsonString());
+//        jsonParsing(getJsonString());
+        dataList = getContactList();
 //        InitializeData();
         RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
         LinearLayoutManager manager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false);
         recyclerView.setLayoutManager(manager); // LayoutManager 등록
         recyclerView.setAdapter(new MyAdapter(dataList));  // Adapter 등록
         return view;
+    }
+
+
+
+    public ArrayList<contact> getContactList() {
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CONTACTS},1);
+        }
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_CONTACTS},1);
+        }
+
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String[] projection = new String[] {
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+        };
+
+        String[] selectionArgs =null;
+        String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+
+        Cursor cursor =  getActivity().getContentResolver().query(uri, projection, null,
+                selectionArgs, sortOrder);
+
+
+        LinkedHashSet<contact> hasList = new LinkedHashSet<>();
+        ArrayList<contact> contactsList;
+
+        if (cursor.moveToFirst()) {
+            do {
+                long photo_id = cursor.getLong(2);
+//                long person = cursor.getLong(3);
+
+                contact myContact = new contact();
+                myContact.setNumber(cursor.getString(0));
+                myContact.setName(cursor.getString(1));
+                myContact.setImage(null);
+                myContact.setEmail("0");
+
+//                if (myContact.getNumber()number.startsWith("01")) {
+                    hasList.add(myContact);
+                    //contactsList.add(myContact);
+                    Log.d("<<CONTACTS>>", "name=" + myContact.getName() + ", phone=" + myContact.getNumber());
+//                }
+
+            } while (cursor.moveToNext());
+        }
+
+        contactsList = new ArrayList<contact>(hasList);
+        for (int i = 0; i < contactsList.size(); i++) {
+            contactsList.get(i).setEmail(null);
+        }
+
+        if (cursor != null) {
+            cursor.close();
+        }
+
+        return contactsList;
     }
 
     private String getJsonString()

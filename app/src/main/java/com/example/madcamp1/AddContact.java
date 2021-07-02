c450ee2cb386c94a293c8b465cbb6bf24dbe3984
fbtngus <702fbtngus@kaist.ac.kr>
2021-07-02 16:51:41 +0900

    Drawables

--- app/src/main/java/com/example/madcamp1/AddContact.java
+++ app/src/main/java/com/example/madcamp1/AddContact.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.FragmentTransaction;
 import android.content.ContentProviderOperation;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.net.Uri;
@@ -15,6 +18,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class AddContact extends AppCompatActivity {
@@ -50,6 +55,22 @@ public class AddContact extends AppCompatActivity {
         }
     }
 
+    public void writeDisplayPhoto(long rawContactId, byte[] photo) {
+        Uri rawContactPhotoUri = Uri.withAppendedPath(
+                ContentUris.withAppendedId(ContactsContract.RawContacts.CONTENT_URI, rawContactId),
+                ContactsContract.RawContacts.DisplayPhoto.CONTENT_DIRECTORY);
+        try {
+            AssetFileDescriptor fd =
+                    getContentResolver().openAssetFileDescriptor(rawContactPhotoUri, "rw");
+            OutputStream os = fd.createOutputStream();
+            os.write(photo);
+            os.close();
+            fd.close();
+        } catch (IOException e) {
+            // Handle error cases.
+        }
+    }
+
     public void onSaveButtonClicked(View v) {
 //        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
 //        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
@@ -82,16 +103,12 @@ public class AddContact extends AppCompatActivity {
                 .build());
 
 //        //------------------------------------------------------ Photo
-//        if (DisplayName != null) {
-//            ops.add(ContentProviderOperation.newInsert(
-//                    ContactsContract.Data.CONTENT_URI)
-//                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-//                    .withValue(ContactsContract.Data.MIMETYPE,
-//                            ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
-//                    .withValue(
-//                            ContactsContract.CommonDataKinds.Photo.PHOTO,
-//                            Thumbnail).build());
-//        }
+//        ContentValues values = new ContentValues();
+//        values.put(ContactsContract.RawContacts.ACCOUNT_TYPE, (byte[]) null);
+//        values.put(ContactsContract.RawContacts.ACCOUNT_NAME, accountName);
+//        Uri rawContactUri = getContentResolver().insert(ContactsContract.RawContacts.CONTENT_URI, values);
+//        long rawContactId = ContentUris.parseId(rawContactUri);
+//        writeDisplayPhoto(rawContactId, photo);
 
         //------------------------------------------------------ Names
         if (DisplayName != null) {
@@ -118,30 +135,6 @@ public class AddContact extends AppCompatActivity {
                     .build());
         }
 
-//        //------------------------------------------------------ Home Numbers
-//        if (HomeNumber != null) {
-//            ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-//                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-//                    .withValue(ContactsContract.Data.MIMETYPE,
-//                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-//                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, HomeNumber)
-//                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
-//                            ContactsContract.CommonDataKinds.Phone.TYPE_HOME)
-//                    .build());
-//        }
-//
-//        //------------------------------------------------------ Work Numbers
-//        if (WorkNumber != null) {
-//            ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-//                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-//                    .withValue(ContactsContract.Data.MIMETYPE,
-//                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-//                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, WorkNumber)
-//                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
-//                            ContactsContract.CommonDataKinds.Phone.TYPE_WORK)
-//                    .build());
-//        }
-
         //------------------------------------------------------ Email
         if (emailID != null) {
             ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
@@ -152,19 +145,6 @@ public class AddContact extends AppCompatActivity {
                     .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
                     .build());
         }
-
-//        //------------------------------------------------------ Organization
-//        if (!company.equals("") && !jobTitle.equals("")) {
-//            ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-//                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-//                    .withValue(ContactsContract.Data.MIMETYPE,
-//                            ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
-//                    .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, company)
-//                    .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
-//                    .withValue(ContactsContract.CommonDataKinds.Organization.TITLE, jobTitle)
-//                    .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
-//                    .build());
-//        }
 
         // Asking the Contact provider to create a new contact
         try {

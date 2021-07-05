8fcd5ab92bd8e8c6b857db605ee9d6aa2aa9c86b
yeji <yejiyoun@sookmyung.ac.kr>
2021-07-05 14:03:10 +0900

    add contact detail

--- app/src/main/java/com/example/madcamp1/ContactAdapter.java
+++ app/src/main/java/com/example/madcamp1/ContactAdapter.java
@@ -1,17 +1,20 @@
 package com.example.madcamp1;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class ContactAdapter extends RecyclerView.Adapter<ContactViewHolder> {
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
 
-    private ArrayList<Contact> myDataList = null;
+    public static ArrayList<Contact> myDataList = null;
 
     ContactAdapter(ArrayList<Contact> dataList)
     {
@@ -19,27 +22,29 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactViewHolder> {
     }
 
     @Override
-    public ContactViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    public ContactAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
     {
         Context context = parent.getContext();
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         //전개자(Inflater)를 통해 얻은 참조 객체를 통해 뷰홀더 객체 생성
         View view = inflater.inflate(R.layout.contact_recyclerview, parent, false);
-        ContactViewHolder viewHolder = new ContactViewHolder(view);
+        ContactAdapter.ViewHolder vh = new ContactAdapter.ViewHolder(view);
 
-        return viewHolder;
+        return vh;
     }
 
     @Override
-    public void onBindViewHolder(ContactViewHolder viewHolder, int position)
+    public void onBindViewHolder(ContactAdapter.ViewHolder viewHolder, int position)
     {
+
         //ViewHolder가 관리하는 View에 position에 해당하는 데이터 바인딩
         viewHolder.imageView.setImageDrawable(myDataList.get(position).getImage());
 //        viewHolder.imageView.setImageResource(myDataList.get(position).getImage());
         viewHolder.name.setText(myDataList.get(position).getName());
         viewHolder.number.setText(myDataList.get(position).getNumber());
         viewHolder.email.setText(myDataList.get(position).getEmail());
+
     }
 
     @Override
@@ -47,5 +52,35 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactViewHolder> {
     {
         //Adapter가 관리하는 전체 데이터 개수 반환
         return myDataList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView name;
+        TextView number;
+        TextView email;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION){
+                        Contact item = myDataList.get(pos);
+                        Intent i = new Intent(v.getContext(), ContactDetailActivity.class);
+                        i.putExtra("contactpos", pos);
+                        v.getContext().startActivity(i);
+                    }
+                }
+            });
+
+            imageView = itemView.findViewById(R.id.imageView2);
+            name = itemView.findViewById(R.id.name);
+            number = itemView.findViewById(R.id.number);
+            email = itemView.findViewById(R.id.email);
+
+        }
     }
 }
